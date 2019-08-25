bc1b56679bdde63f4fe5cf4e4c37ed4504a0f107
Kyle Laster <kyle.laster@gmail.com>
2019-08-24 23:05:29 -0500

    routes added for blog and clientPortal

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 // Internal Dependencies
 import LoadingPage from './pages/LoadingPage';
+import {ROUTES} from './constants/routes';
 
 // Local Variables
 const LazyHomePageRoute = React.lazy(() =>
   import('./pages/LandingPage')
 );
+const LazyBlogRoute = React.lazy(() => import('./pages/Blog'))
+const LazyClientPortalRoute = React.lazy(() => import ('./pages/ClientPortal'));
 
 const HomePage = (props: any) => <LazyHomePageRoute {...props} />;
+const BlogPage = (props: any) => <LazyBlogRoute {...props} />;
+const ClientPortal = (props: any) => <LazyClientPortalRoute {...props} />;
 
 const App = () => {
   return (
@@ -19,6 +24,8 @@ const App = () => {
         <React.Suspense fallback={<LoadingPage />}>
           <div>
             <Route path="/" exact component={HomePage} />
+            <Route path={ROUTES.BLOG} component={BlogPage} />
+            <Route path={ROUTES.CLIENT_PORTAL} component={ClientPortal} />
           </div>
         </React.Suspense>
       </BrowserRouter>
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+  BLOG: '/blog',
+  CLIENT_PORTAL: '/client_portal',
+}
