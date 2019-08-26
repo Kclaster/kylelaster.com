d8d40cc54ff22ebf71107d7db3f553b4f09fa605
Kyle Laster <kyle.laster@gmail.com>
2019-08-25 22:09:16 -0500

    linter added

--- src/components/GlossedWindow/index.tsx
+++ src/components/GlossedWindow/index.tsx
@@ -1,32 +1,31 @@
 // External Dependencies
-import {css} from 'emotion';
+import { css } from 'emotion';
 import React from 'react';
 
 // Internal Dependencies
-import { BaseSizes, BASE_SIZES } from '../../constants/sizes'
+import { BASE_SIZES, BaseSizes } from '../../constants/sizes';
 
 // Local Typings
 interface Props {
-  width: BaseSizes
-};
+  width: BaseSizes;
+}
 
 // Local Variables
-const getStyle = (props: Props) => css({
-  width: getWidth(props.width),
-  boxShadow: 'inset 0 0 0 3000px rgba(255,255,255,0.3);',
-  height: '500px',
-  position: 'relative',
-});
+const getStyle = (props: Props) =>
+  css({
+    boxShadow: 'inset 0 0 0 3000px rgba(255,255,255,0.3);',
+    height: '500px',
+    position: 'relative',
+    width: getWidth(props.width),
+  });
 
-function getWidth( width : BaseSizes) {
+function getWidth(width: BaseSizes) {
   return BASE_SIZES[width];
 }
 
 // Component Definition
-const GlossedWindow: React.FC<Props> = (props) => {
-  return (
-    <div className={getStyle(props)} />
-  )
-}
+const GlossedWindow: React.FC<Props> = props => {
+  return <div className={getStyle(props)} />;
+};
 
 export default GlossedWindow;
