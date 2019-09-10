1642ffddfca52881ef03aa1fd04a02116259c474
Kyle Laster <kyle.laster@gmail.com>
2019-09-09 21:47:43 -0500

    minor

---
+++ src/components/AppearTyping/UnderScore.tsx
@@ -0,0 +1,29 @@
+// External Dependencies
+import styled from '@emotion/styled';
+import React from 'react';
+
+// Internal Dependencies
+import { css } from 'emotion';
+import { BASE_COLORS } from '../../constants/styles';
+
+// Local Typings
+interface Props {
+  shouldRender: boolean;
+}
+
+// Local Variables
+const getStyle = ({ shouldRender }: Props) => css({
+  backgroundColor: shouldRender ? BASE_COLORS.WHITE : 'transparent',
+  display: 'inline-block',
+  height: '1em',
+  width: '.5em',
+});
+
+// Component Definition
+const UnderScore: React.FC<Props> = (props) => {
+  return (
+    <div className={getStyle(props)} />
+  );
+};
+
+export default UnderScore;
--- src/components/AppearTyping/index.tsx
+++ src/components/AppearTyping/index.tsx
@@ -1,11 +1,11 @@
 // External Dependencies
+import { css } from 'emotion';
 import React from 'react';
 
 // Internal Dependencies
-import { css } from 'emotion';
-import Text from '../../components/Text';
 import { BasePercents, SPACE_SIZES } from '../../constants/sizes';
-
+import Text from '../Text';
+import UnderScore from './UnderScore';
 // Local Typings
 interface Props extends WrapperProps {
   children: string;
@@ -13,6 +13,12 @@ interface Props extends WrapperProps {
 
 interface WrapperProps {
   width?: BasePercents | 'auto';
+}
+
+interface State {
+  renderedLetters: string[];
+  shouldRender: boolean;
+  splitChildren: string[];
 }
 
 // Local Variable
@@ -24,20 +30,56 @@ const getStyle = ({
 });
 
 // Component Definition
-const AppearTyping: React.FC<Props> = ({
-  children,
-  ...props
-}) => {
-  const splitChildren = children.split('');
-  console.log(splitChildren);
+class AppearTyping extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      renderedLetters: [],
+      shouldRender: false,
+      splitChildren: [],
+    };
+  }
 
-  return (
-    <div className={getStyle(props)}>
-      <Text textIndent="30px" fontSize="sm">
-        {children}
-      </Text>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.myInterval = window.setInterval(this.handleMoveCharacters, 12);
+    this.underscoreInterval = window.setInterval(this.handleToggleUnderscore, 250);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval);
+  }
+
+  handleMoveCharacters = () => {
+    const nextLetter = this.splitChildren.shift();
+    if (nextLetter) {
+      this.setState({
+        renderedLetters: [...this.state.renderedLetters, nextLetter],
+      });
+    }
+  }
+
+  handleToggleUnderscore = () => {
+    this.setState({
+      shouldRender: !this.state.shouldRender,
+    });
+  }
+
+  render() {
+    return (
+      <div className={getStyle(this.props)}>
+        <Text textIndent="30px" fontSize="sm">
+          {this.state.renderedLetters.join('')}
+          {' '}
+          <UnderScore shouldRender={this.state.shouldRender} />
+        </Text>
+      </div>
+    );
+  }
+  // tslint:disable-next-line:new-parens
+  myInterval: number | undefined;
+
+  splitChildren = this.props.children.split('');
+  underscoreInterval: number | undefined;
+}
 
 export default AppearTyping;
